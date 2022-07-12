8585283f5a2a8bde6b23275165dd907a66e47b70
Brenth Andrew J Miras <brenthmiras2@gmail.com>
2022-07-12 15:22:36 +0800

    Add basic store using vuex

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
+import store from './store'
 import BootstrapVue from 'bootstrap-vue-3';
 
 // Import Bootstrap and BootstrapVue CSS files (order is important)
 import 'bootstrap/dist/css/bootstrap.css';
 
-createApp(App).use(router).use(BootstrapVue).mount('#app');
+createApp(App).use(router).use(store).use(BootstrapVue).mount('#app');
---
+++ src/store/Cat.js
@@ -0,0 +1,21 @@
+const initialState = {
+  breedList: {},
+  catList: {},
+  currentCat: {},
+};
+
+const state = () => (initialState);
+
+const actions = {};
+
+const mutations = {};
+
+const getters = {};
+
+export default {
+  namespaced: true,
+  mutations,
+  actions,
+  getters,
+  state,
+};

0b37a847cc4a02233348da0944b816db021aa554
Brenth Andrew J Miras <brenthmiras2@gmail.com>
2022-07-12 19:41:37 +0800

    Cat list - Show placeholder only when no cats

--- src/components/CatList.vue
+++ src/components/CatList.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="row">
-    <div class="col-12" style="margin-bottom: 20px;">
+
+    <div class="col-12" style="margin-bottom: 20px;" v-if="!catList.length">
       No cats available
     </div>
 
